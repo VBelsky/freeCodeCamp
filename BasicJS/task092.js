dbf21fd6227b8048709871ee59eff63731b1b2b1
VitalyBelsky <vitaly.belsky@startmatter.com>
2022-02-18 21:43:10 +0300

    Manipulating Complex Objects

---
+++ BasicJS/task092.js
@@ -0,0 +1,23 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Symplex Method",
+    "title": "Lovecraftiana",
+    "release_year": 2020,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ]
+  }
+];
